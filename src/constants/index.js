117ba4326c82ead31d12fc666ac7deab68c62a4c
Garima Jaiswal <garima21csu194@ncuindia.edu>
2024-03-19 10:17:07 +0530

    portFoilio

---
+++ src/constants/index.js
@@ -0,0 +1,195 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Java",
+      icon: web,
+    },
+    {
+      title: "Html",
+      icon: html,
+    },
+    {
+      title: "CSS",
+      icon: css,
+    },
+    {
+      title: "Javascript",
+      icon: javascript,
+    },
+    {
+      title: "React JS",
+      icon: reactjs,
+    },
+    {
+      title: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      title: "Mongo DB",
+      icon: mongodb,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+  
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+  
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+   
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+   
+   
+  ];
+  
+  const experiences = [
+   
+    {
+      title: "Front End Developer",
+      company_name: "Suven Consultants and Technology Pvt.Ltd",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "June 2023 - July 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+   
+  
+  ];
+  
+  const testimonials = [
+    {
+      testimonial: [
+        "garima21csu194@ncuindia.edu ",
+        " +(91)7982924258  ",
+        
+      
+      ],
+      // name: "Sara Lee",
+      // designation: "gggg",
+      // company: "Acme Co",
+      // image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+  
+  ];
+  
+  const projects = [
+    {
+      name: " E-Commerce Application",
+      description: "Developed an e-commerce application, a digital platform for seamless online transactions, featuring secure payments and order tracking. The user-friendly interface enhances the overall shopping experience, making it convenient and efficient for buyers and sellers alike.",
+     
+        
+      tags: [
+        {
+          name: "Html",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: "https://pasinfotech.com/wp-content/uploads/2019/03/e-commerce-website.png",
+      source_code_link: "https://jaiswalgarima.netlify.app/",
+    },
+    {
+      name: "Polling App",
+      description:
+        "Developed a revolutionary polling app project to simplify opinion gathering for decision-making. Seamlessly engage users, analyze real-time results, and  redefining the feedback process for impactful decision support",
+      tags: [
+        {
+          name: "React JS",
+          color: "blue-text-gradient",
+        },
+     
+        {
+          name: "Node JS",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Express JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Mongo DB",
+          color: "blue-text-gradient",
+        },
+            
+      ],
+      image: "https://images.ctfassets.net/rvt0uslu5yqp/4sL7hMUKGMwWikyJWp6iHQ/3bedd3ce600457f34428746e22fcbad8/Mentimeter_Applications_Web_Live-Polling_HeroImage_2021__1_.png?w=1200&h=630&q=90&fm=jpg",
+      source_code_link: "https://github.com/GarimaJaiswal12/Polling-App",
+    },
+   
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
